0c862824df49b71a95888d913f9cf9c2184b5a3e
Alexander Malandin <SlendermanGame18@mail.ru>
2025-04-17 12:17:46 +0300

    hw6

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -1,15 +1,20 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
     [SerializeField] private float speedMove = 20f;
     [SerializeField] private float speedJump = 10f;
     [SerializeField] private float rayDistance = 3f;
+    private AudioSource runningSound;
+    private AudioSource jumpSound;
     public LayerMask groundLayer;
     private SpriteRenderer pl;
     private Rigidbody2D rb;
     private PlayerState state;
     private bool lookRight;
+    private bool playing = true;
+    public bool Playing { get => playing; set => playing = value; }
 
     public PlayerState State { get => state; set => state = value; } 
     public Rigidbody2D Rb { get => rb; set => rb = value; }
@@ -21,34 +26,39 @@ public class MovementController : MonoBehaviour
         state = PlayerState.Idle;
         lookRight = true;
         pl = GetComponent<SpriteRenderer>();
+        runningSound = GetComponents<AudioSource>()[0];
+        jumpSound = GetComponents<AudioSource>()[1];
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (playing)
+        {
 
-        // runing logic
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
-        {
-            Run();
-        }
-        // idle logic
-        else if (!Input.anyKey && state != PlayerState.Jumping)
-        {
-            Idle();
-        }
-        // jumpic logic
-        if (state != PlayerState.Jumping && Input.GetKey(KeyCode.Space))
-        {
-            Jump();
-        }
+            // runing logic
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+            {
+                Run();
+            }
+            // idle logic
+            else if (!Input.anyKey && state != PlayerState.Jumping)
+            {
+                Idle();
+            }
+            // jumpic logic
+            if (state != PlayerState.Jumping && Input.GetKey(KeyCode.Space))
+            {
+                Jump();
+            }
 
-        if (IsGrounded() && rb.linearVelocityY <= 0.1)
-        {
-            if (!Input.anyKey)
-                state = PlayerState.Idle;
-            else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
-                state = PlayerState.Running;
+            if (IsGrounded() && rb.linearVelocityY <= 0.1)
+            {
+                if (!Input.anyKey)
+                    state = PlayerState.Idle;
+                else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+                    state = PlayerState.Running;
+            }
         }
     }
 
@@ -69,10 +79,15 @@ public class MovementController : MonoBehaviour
     {
         rb.AddForce(new Vector2(0, speedJump), ForceMode2D.Impulse);
         state = PlayerState.Jumping;
+        jumpSound.Play();
     }
 
     private void Run()
     {
+        if (!runningSound.isPlaying)
+        {
+            runningSound.Play();
+        }
         rb.linearVelocityX = Input.GetAxis("Horizontal") * speedMove;
         if (state != PlayerState.Jumping)
             state = PlayerState.Running;
@@ -81,6 +96,7 @@ public class MovementController : MonoBehaviour
             (Input.GetAxis("Horizontal") < 0 && lookRight)
            )
             Flip();
+        
     }
 
     private void Idle()
